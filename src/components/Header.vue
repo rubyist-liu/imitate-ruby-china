68d5a82270b43bd604bb9a6c5bc524c227e23b1d
Zhenyuan Liu <rubyist.liu@gmail.com>
2017-10-01 13:58:58 +0800

    使用scss

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <nav>
+      <div class="navbar-header">
+        <b>Ruby</b> China
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .header {
+    nav {
+      .navbar-header {
+        b {
+          color: red;
+        }
+      }
+    }
+  }
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div is="app-header"></div>
+  </div>
+</template>
+
+<script>
+import AppHeader from './Header.vue'
+export default {
+  name: 'index',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
